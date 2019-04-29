eafacdbc8f661fceb88453ee1f33841b260510be
Alp Cehri <cehri.alp@gmx.com>
2019-04-29 22:22:42 +0200

    Initial commit

---
+++ app/src/main/java/com/cehri/alp/dicee/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cehri.alp.dicee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.roll_button);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.diceleft);
+        final ImageView rightDice = (ImageView) findViewById(R.id.diceRight);
+
+        final int[] diceArray = { R.drawable.dice1,
+                            R.drawable.dice2,
+                            R.drawable.dice3,
+                            R.drawable.dice4,
+                            R.drawable.dice5,
+                            R.drawable.dice6};
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumber = new Random();
+                int numberLeft = randomNumber.nextInt(6);
+                int numberRight = randomNumber.nextInt(6);
+
+
+                leftDice.setImageResource(diceArray[numberLeft]);
+                rightDice.setImageResource(diceArray[numberRight]);
+            }
+        });
+    }
+}
